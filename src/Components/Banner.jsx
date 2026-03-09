985341874786c65a5a3eceeb8bcb317cb7f972cb
Veeru Madhesiya <veerumadheshiya009@gmail.com>
2026-03-09 10:09:00 +0530

    Update Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
       <div className='bg-black inset-0 opacity-65 absolute'></div>
       <div className='text-white flex-col flex items-center justify-center px-4 lg:px-0 text-center max-w-7xl mx-auto z-20'>
         <h2 className='lg:text-6xl text-4xl font-bold mb-6'>Ready to Start Your Adventure?</h2>
-        <p className='text-xl mb-8'>Book your dream vacation today and create unforgettable memories.</p>
+        <p className='text-xl mb-8'>Book your dreams vacation today and create unforgettable memories.</p>
         <button className='bg-red-500 px-3 py-2 rounded-md text-white'>Start Planning</button>
       </div>
     </div>
